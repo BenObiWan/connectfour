f27a7a17735db03709264dd1e55f3fedf79916e1
BenObiWan <benobiwan@gmail.com>
2011-09-05 23:20:10 +0200

    Renamed ConnectFourClientSidePlayer to ClientSideConnectFourPlayerImpl.

--- src/connectfour/src/game/connectfour/client/ClientSideConnectFourPlayerImpl.java
+++ src/connectfour/src/game/connectfour/client/ClientSideConnectFourPlayerImpl.java
@@ -19,7 +19,7 @@ import game.connectfour.event.ConnectFourGameEventType;
 import game.gameclient.AbstractClientSidePlayer;
 import game.gameclient.LocalGameClient;
 
-public final class ConnectFourClientSidePlayer
+public final class ClientSideConnectFourPlayerImpl
 		extends
 		AbstractClientSidePlayer<IConnectFourConfiguration, AbstractConnectFourGameEvent, ConnectFourClientSideGame, IClientSideConnectFourPlayer, IConnectFourPlayerConfiguration, IConnectFourClientSidePlayerObserver>
 		implements IClientSideConnectFourPlayer
@@ -37,7 +37,7 @@ public final class ConnectFourClientSidePlayer
 	 * @param localGameClient
 	 *            the {@link LocalGameClient}.
 	 */
-	protected ConnectFourClientSidePlayer(final int iPlayerId,
+	protected ClientSideConnectFourPlayerImpl(final int iPlayerId,
 			final String strName, final IGameServer server,
 			final LocalGameClient localGameClient)
 	{
